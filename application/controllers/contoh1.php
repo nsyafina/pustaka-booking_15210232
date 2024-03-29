51e2338758c9fa4e724dbd1173ba15fdf0b25f87
nsyafina <najlasfina13@gmail.com>
2024-03-29 21:14:23 +0700

    menambahkan .editorconfig,.gitignore,.htaccess,application/,composer.json,index.php,license.txt,readme.rst,system/

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+public function index()
+{
+echo "<h1>Perkenalkan guys</h1>";
+echo" Allo guys.. Nama saya Siti Najla Syafina, tinggal di daerah Cibitung, hobi yang saya sukai adalah Tidur hahaha";
+}
+}
+?>
