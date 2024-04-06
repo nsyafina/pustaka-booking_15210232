3c47dbdc4bea1cbd623ca8f498318befcd55f8a1
najlasfina13@gmail.com <najlasfina13@gmail.com>
2024-04-06 21:08:39 +0700

    Tugas pertemuan 03 dan pertemuan 04

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Siti Najla</li>
+		<li>Nama Belakang : Syafina</li>
+	</ul>
+
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li> Jl. Dahlia, Cibitung</li>
+	</ul>
+
+	<h4>Tempat, Tanggal Lahir</h4>
+	<ul type="none">
+		<li>Jakarta, 13 Agustus 2002</li>
+	</ul>
+
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li>Tiidur</li>
+		<li>Tidur Juga</li>
+	</ul>
+</section>
